5d608e36ad3900c9993fc3c50e510fc50d8f6a02
omolinsc <oriol.molins@bootcamp-upgrade.com>
2022-12-24 10:01:12 +0100

    FEAT: iteration#3 done

--- index.js
+++ index.js
@@ -77,25 +77,40 @@ console.log("");
 console.log("- - - - - ITERATION # 3 SPREAD OPERATOR - - - - -");
 
 // 3.1 Dado el siguiente array, crea una copia usando spread operators.
-// const pointsList = [32, 54, 21, 64, 75, 43]
+const pointsList = [32, 54, 21, 64, 75, 43]
+
+const pointsListCopy = [...pointsList];
+console.log("El Spread Operator del 3.1 es:", pointsListCopy);
 
 // 3.2 Dado el siguiente objeto, crea una copia usando spread operators.
-// const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
+const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
+
+const toyCopy = {...toy};
+console.log("El Spread Operator del 3.2 es:", toyCopy);
 
 // 3.3 Dado los siguientes arrays, crea un nuevo array juntandolos usando 
 // spread operatos.
-// const pointsList = [32, 54, 21, 64, 75, 43];
-// const pointsLis2 = [54,87,99,65,32];
+const pointsList0 = [32, 54, 21, 64, 75, 43];
+const pointsList2 = [54,87,99,65,32];
+
+const pointsListConcat = [...pointsList0,...pointsList2];
+console.log("El Spread Operator del 3.3 es:", pointsListConcat);
 
 // 3.4 Dado los siguientes objetos. Crea un nuevo objeto fusionando los dos 
 // con spread operators.
-// const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
-// const toyUpdate = {lights: 'rgb', power: ['Volar like a dragon', 'MoonWalk']}
+const toy0 = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
+const toyUpdate = {lights: 'rgb', power: ['Volar like a dragon', 'MoonWalk']};
+
+const newToy = {...toy0,...toyUpdate};
+console.log("El Spread Operator del 3.4 es:", newToy);
 
 // 3.5 Dado el siguiente array. Crear una copia de él eliminando la posición 2 
 // pero sin editar el array inicial. De nuevo, usando spread operatos.
-// const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja'];
+const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja'];
 
+const colorsCopy = [...colors];
+colorsCopy.splice(2,1);
+console.log("El Spread Operator del 3.5 es:", colorsCopy);
 
 
 //* Iteración #4: Map
